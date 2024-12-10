76903d3b2373a98c2de174f487e3728e517c67a6
lnaniwowen <155816038+lnaniwowen@users.noreply.github.com>
2024-12-10 21:08:08 +0800

    Addition of MainScreen Frame

--- src/projectoop/Main.java
+++ src/projectoop/Main.java
@@ -2,6 +2,10 @@ package projectoop;
 
 public class Main {
     public static void main(String[] args) {
+    ConnectDB clConnect = new ConnectDB();
+    MainScreen jfMS = new MainScreen();
+
+    jfMS.setVisible(true);
     }
     
 }
